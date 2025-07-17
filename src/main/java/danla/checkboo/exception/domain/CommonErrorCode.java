a4e5ecc2c37a766c06499cde7c8d37b29b007f82
SeongHwan <aiya0422@gmail.com>
2025-07-17 20:20:28 +0900

    feat: 에러 처리 개선
    - Valid 에러 처리에 따른 로직 추가

--- src/main/java/danla/checkboo/exception/domain/CommonErrorCode.java
+++ src/main/java/danla/checkboo/exception/domain/CommonErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
 
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류"),
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "입력값 오류.");
 
     private final HttpStatus httpStatus;
     private final String message;
--- src/main/java/danla/checkboo/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/danla/checkboo/exception/handler/GlobalExceptionHandler.java
@@ -4,19 +4,27 @@ import danla.checkboo.exception.domain.CommonErrorCode;
 import danla.checkboo.exception.domain.ErrorCode;
 import danla.checkboo.exception.domain.RestApiException;
 import danla.checkboo.exception.dto.ErrorResponse;
+import jakarta.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RestApiException.class)
-    public ResponseEntity<Object> handleCustomException(RestApiException e) {
-        ErrorCode errorCode = e.getErrorCode();
+    public ResponseEntity<Object> handleCustomException(RestApiException ex) {
+        ErrorCode errorCode = ex.getErrorCode();
         return handleException(errorCode);
     }
 
@@ -25,6 +33,37 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         log.warn("handleAllException", ex);
         ErrorCode errorCode = CommonErrorCode.INTERNAL_SERVER_ERROR;
         return handleException(errorCode);
+    }
+
+    @Nonnull
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            @Nonnull MethodArgumentNotValidException ex,
+            @Nonnull HttpHeaders headers,
+            @Nonnull HttpStatusCode status,
+            @Nonnull WebRequest request) {
+        log.warn("handleIllegalArgument", ex);
+        ErrorCode errorCode = CommonErrorCode.INVALID_PARAMETER;
+        return handleException(ex, errorCode);
+    }
+
+    private ResponseEntity<Object> handleException(MethodArgumentNotValidException ex, ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(makeErrorResponse(ex, errorCode));
+    }
+
+    private ErrorResponse makeErrorResponse(MethodArgumentNotValidException ex, ErrorCode errorCode) {
+        List<ErrorResponse.ValidationError> validationErrorList = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(ErrorResponse.ValidationError::of)
+                .collect(Collectors.toList());
+
+        return ErrorResponse.builder()
+                .code(errorCode.name())
+                .message(errorCode.getMessage())
+                .errors(validationErrorList)
+                .build();
     }
 
     private ResponseEntity<Object> handleException(ErrorCode errorCode) {
