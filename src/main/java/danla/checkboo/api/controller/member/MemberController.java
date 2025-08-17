55505f6ab700963152f902a2ce966199c767b858
SeongHwan <aiya0422@gmail.com>
2025-08-17 12:40:24 +0900

    feat: 파라미터 어노테이션 수정

--- src/main/java/danla/checkboo/api/controller/member/MemberController.java
+++ src/main/java/danla/checkboo/api/controller/member/MemberController.java
@@ -2,6 +2,7 @@ package danla.checkboo.api.controller.member;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import danla.checkboo.api.controller.member.dto.SignupRequest;
@@ -16,7 +17,7 @@ public class MemberController {
 	private final MemberService service;
 
 	@PostMapping("/members/signup")
-	public ResponseEntity<Object> signup(@Valid SignupRequest request) {
+	public ResponseEntity<Object> signup(@RequestBody @Valid SignupRequest request) {
 		service.signup(request.email(), request.password(), request.username());
 		return ResponseEntity.noContent().build();
 	}
